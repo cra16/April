da4adfe9d0f3ecd931f29da42c68501859255fc4
LeeDongYoung <dyl159@naver.com>
2016-02-12 21:12:23 +0900

    비교과과정 추가 삭제입니다( area colomn을 꼭 등록해주세요)

--- php/Admin_Nonsub_Handling.php
+++ php/Admin_Nonsub_Handling.php
@@ -1,57 +1,72 @@
 <?php
 // Session start 
 session_start();
+    
+//DB connection
+include 'Config_DB.php';
+    
+// Fetch the data.
+$field = $_POST['field'];            // 학회 or 캠프
+$course = $_POST['course'];    // 학부
+$area = $_POST['area'];          // 항목 ex) 인문사회
+$name = $_POST['name'];       // 이름
+$mode = $_POST['mode'];      // CRUD MODE
+
+// Read(default)
+if($mode == 0){
+    // FIXME: 서버로 올릴경우 변경 필수!
+    $db_name  = 'april';
+    $hostname = '127.0.0.1';
+    $username = 'root';
+    $password = 'gksehdeo357';
+
+    // connect to the database
+    $dbh = new PDO("mysql:host=$hostname;dbname=$db_name", $username, $password);
+    $sql = 'SELECT * FROM nonsubject';
+    // use prepared statements, even if not strictly required is good practice
+    $stmt = $dbh->prepare( $sql );
+    // execute the query
+    $stmt->execute();
+    // fetch the results into an array
+    $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+    // convert to json
+    $json = json_encode( $result );
+    //close Database
+    $dbh = null;
+    // echo the json string
+    echo $json;
+}
 
 // Insert
-if ($mode == 1)  
-{      
-    //DB connection
-    include 'Config_DB.php';
-        
-    // Fetch the data.
-    $field = $_POST['p_field'];
-    $course = $_POST['p_course'];
-    $area = $_POST['p_area'];
-    $name = $_POST['p_name'];
-
-
-    // Chech if it already exists.
-    $rep = 0;    //repetition 약자
-    $rep = "SELECT * FROM nonsubject WHERE field = '$field' AND course = '$course' AND area = '$area' AND name = '$name'";
-      
-    // Insert the data.   
-    if($rep != 0){                            
-        mysql_query("INSERT INTO `nonsubject`(field, course, area, name) VALUES ('$field','$course','$area','$name')");   
-    }     
-    else {
-          echo "Same data exists! " . $link->error;
+if($mode == 1){
+    // Check if it already exists.
+    $sql  = "SELECT * FROM nonsubject WHERE field = '$field' AND course = '$course' AND area = '$area' AND name = '$name'";
+    $result = $link->query($sql);
+    if ($result->num_rows > 0){
+        echo -1; //중복된 결과
+    }
+    else{        // Insert the data.   
+        $sql = "INSERT INTO `nonsubject`(field, course, area, name) VALUES ('$field','$course','$area','$name')";
+        if ($link->query($sql) === TRUE) {
+        echo 1; //입력 성공
+        } else {
+        echo -1; //입력 실패
+        }
     }            
 }
+      
 
 // Remove
-if ($mode == -1)
-{      
-    //DB connection
-    include 'Config_DB.php';
-
-    // Fetch the data.
-    $field = $_POST['p_field'];
-    $course = $_POST['p_course'];
-    $area = $_POST['p_area'];
-    $name = $_POST['p_name'];
-
+if($mode == -1){
     // Remove the data.                                  
-    mysql_query("DELETE FROM nonsubject WHERE field = '$field' AND course = '$course' AND area = '$area' AND name = '$name''");                
+    $sql = "DELETE FROM nonsubject WHERE field = '$field' AND course = '$course' AND area = '$area' AND name = '$name'";                
 
-    // Chech if it is deleted successfully.
-    $del = 0;
-    $del = "SELECT * FROM nonsubject WHERE field = '$field' AND course = '$course' AND area = '$area' AND name = '$name'";
-    if($del != 0){
-        echo "Deletion failed! " . $link->error;
+    if ($link->query($sql) === TRUE) {
+    echo 1;
+    } else {
+    echo -1;
     }
 }
-
-
 
 /*
 // Update
@@ -74,9 +89,8 @@ if ($mode == 2)
 */
 
 
-
 $link->close();
-header("Location: Admin_Page.php");    
-exit();
- 
+// header("Location: Admin_Page.php");    
+// exit();
+
 ?>
