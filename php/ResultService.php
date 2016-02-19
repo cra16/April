b5c75713dcfa3a010f91896d799c2dbeccf49b5d
world <yyll9933@gmail.com>
2016-02-20 02:00:50 +0900

    파일명 잘못된것 바꾸고, 지원중복 여부 판별하는기능 추가

--- php/ResultService.php
+++ php/ResultService.php
@@ -1,38 +1,23 @@
 <?php
 
 // Connect with DB
-<<<<<<< HEAD
 require_once("Config_DB.php");
 $db = new DB_Control();
 $link = $db->DBC();
 
 print_r($_POST);
-echo "er5yeyry";
-echo "11111111";
-echo $_POST['foundation'];
-echo "2222222";
-foreach($_POST['chk_info'] as $check) {
-    echo $check;
-    }
-    echo "333333";
-foreach($_POST['data_info'] as $check) {
-    echo $check;
-    }
-=======
-require_once('Config_DB.php');
->>>>>>> d1fd598c8b468bb2be635a898c899ec4ab75b4f9
 
 // check second field and save result
 if(!empty($_POST['chk_info'])) {
-	$chk = array();
-	$i = 0
-	$nonsubject
+    $chk = array();
+    $i = 0;
 
-	foreach($_POST['chk_info'] as $check) {
+
+    foreach($_POST['chk_info'] as $check) {
 
             $chk[$i] =  $check;
-			$i++;
-	}
+            $i++;
+    }
 
     $condition = "SELECT * FROM camp UNION SELECT * FROM academy ";
 
@@ -54,10 +39,16 @@ if(!empty($_POST['chk_info'])) {
 
     $nonsubject = "";
     $nonarea = "";
-    foreach($_POST['chk_info'] as $check) {
+    $nonyear = "";
+
+    foreach($_POST['chk_info'] as $check){
     $nonsubject = $nonsubject."/".$non_name[$check];
     $nonarea = $nonarea."/".$non_area[$check];
     }
+    foreach($_POST['year_array'] as $year){
+    $nonyear = $nonyear."/".$year;   
+    }
+    $nonyear = "$nonyear";
     
 
 }
@@ -69,23 +60,25 @@ if(!empty($_POST['data_info'])) {
     }
 
 $id = "yyll9933";//$_SESSION['USER_NAME'];
-$foundation = $_POST['foundation'];
+$foundation = "기초역량";
 
 //check the information
 
-// $qry = "SELECT * FROM `application` WHERE his_id = '$id'";
-// $data = $link->query($qry);
-// echo "+++++++".$data['kind']."-------"; 
-// if ($data&&$data["kind"] === $foundation) {
-//     if($data["status"]!="지원")
-//     {
-//         echo "Your status is already checking";
-//         //header("Location: ../php/Service.php");
-//     }
-//     $sql = "UPDATE `application` SET his_id = $id, non_sub = $nonsubject, kind = $foundation, area = $nonarea, status = '지원' WHERE id = $id AND kind = $foundation";       
-// } else {
-   $sql = "INSERT INTO `application`(his_id,non_sub,kind,area,status) VALUES('$id','$nonsubject','$foundation','$nonarea','지원' )";
-//}
+$qry = "SELECT * FROM `application` WHERE his_id = '$id' AND kind = '$foundation'";
+$datas = $link->query($qry);
+
+foreach($datas as $data){
+if ($data) {
+    if($data["status"]!="지원")
+    {
+        echo "Your status is already checking";
+    }
+    $sql = "UPDATE `application` SET his_id = '$id', non_sub = '$nonsubject', kind = '$foundation', area = '$nonarea', status = '지원',year = '$nonyear' WHERE his_id = '$id' AND kind = '$foundation'";       
+    header("Location: ../php/Service.php");
+} else {
+   $sql = "INSERT INTO `application`(his_id,non_sub,kind,area,status,year) VALUES('$id','$nonsubject','$foundation','$nonarea','지원','$nonyear')";
+}
+}
 
 //making insert query 
 
